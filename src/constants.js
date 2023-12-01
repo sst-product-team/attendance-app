e352944d08657389beb73a2c0223b65251d6c95d
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-02 03:25:26 +0530

    created github manual build workflow for android

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const domain_URL = process.env['domain_URL'];
-const APP_VERSION = '0.3.6';
+const APP_VERSION = process.env['APP_VERSION'];
 const JWT_SIGNATURE = process.env['JWT_SIGNATURE'];
 
 export {domain_URL};
