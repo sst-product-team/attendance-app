5fce716d84cefca4430c54f0d9c1381758b59d3f
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-05 01:41:35 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-// const domain_URL = process.env['domain_URL'];
-const domain_URL = 'https://c5fe-106-51-13-146.ngrok-free.app';
+const domain_URL = process.env['domain_URL'];
 const APP_VERSION = process.env['APP_VERSION'];
 const JWT_SIGNATURE = process.env['JWT_SIGNATURE'];
+
 export {domain_URL};
 export {APP_VERSION};
 export {JWT_SIGNATURE};
