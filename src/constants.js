d51ac224baf3d3278bb00a03476e5c4749d4a74f
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-08 21:59:01 +0530

    setup for VERSION_NAME done

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 const domain_URL = process.env['domain_URL'];
 const APP_VERSION = process.env['APP_VERSION'];
 const JWT_SIGNATURE = process.env['JWT_SIGNATURE'];
+const VERSION_NAME = process.env['VERSION_NAME'];
 
 export {domain_URL};
 export {APP_VERSION};
 export {JWT_SIGNATURE};
+export {VERSION_NAME};
