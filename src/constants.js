98511e1d67cb6701b0f53bec7c7912f6a05f291f
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-09 03:01:13 +0530

    added SKIP_DEVELOPER_MODE_CHECK

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,9 @@ const domain_URL = process.env['domain_URL'];
 const APP_VERSION = process.env['APP_VERSION'];
 const JWT_SIGNATURE = process.env['JWT_SIGNATURE'];
 const VERSION_NAME = process.env['VERSION_NAME'];
-
+const SKIP_DEVELOPER_MODE_CHECK = process.env['SKIP_DEVELOPER_MODE_CHECK'];
 export {domain_URL};
 export {APP_VERSION};
 export {JWT_SIGNATURE};
 export {VERSION_NAME};
+export {SKIP_DEVELOPER_MODE_CHECK};
--- src/utils/checkValidity.js
+++ src/utils/checkValidity.js
@@ -1,6 +1,10 @@
 import JailMonkey from 'jail-monkey';
+import {SKIP_DEVELOPER_MODE_CHECK} from '../constants';
 
 const checkValidity = async () => {
+  if(SKIP_DEVELOPER_MODE_CHECK){
+    return false;
+  }
   const DevMode = await JailMonkey.isDevelopmentSettingsMode(); // check if developer is on
   // const TrustFall = JailMonkey.trustFall(); // check for TrustFall
   // const DebugMode = await JailMonkey.isDebuggedMode(); // Check if debug mode is on
