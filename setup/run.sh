d3dc6f711a2cf51ec29eebd3b256112689b47f97
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-29 14:00:56 +0530

    changed android version to 1.1.0

--- setup/run.sh
+++ setup/run.sh
@@ -1,2 +1,2 @@
-cat ./CodePush.java > node_modules/react-native-code-push/android/app/src/main/java/com/microsoft/codepush/react/CodePush.java
+cat ./setup/CodePush.java > node_modules/react-native-code-push/android/app/src/main/java/com/microsoft/codepush/react/CodePush.java
 echo "Setup run complete"
